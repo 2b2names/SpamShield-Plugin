411e370eaa3dd283beaaa2b9f003535f88060edc
2b2t_names <minecraftdrpurple1@outlook.com>
2025-12-30 15:56:30 +0300

    Update SpamShield.java

--- src/main/java/me/tiger/spamShield/SpamShield.java
+++ src/main/java/me/tiger/spamShield/SpamShield.java
@@ -1,17 +1,30 @@
-package me.tiger.spamShield;
+package me.tiger.spamshield;
 
+import me.tiger.spamshield.listener.ChatListener; // <- correct import
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SpamShield extends JavaPlugin {
 
+    private static SpamShield instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        saveDefaultConfig();
+
+        // Register chat listener
+        getServer().getPluginManager().registerEvents(new ChatListener(), this);
+
+        getLogger().info("SpamShield enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("SpamShield disabled!");
+    }
+
+    public static SpamShield getInstance() {
+        return instance;
     }
 }
