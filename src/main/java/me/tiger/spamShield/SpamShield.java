9239124a0e39e4c2e4f180c64cf0f59cb3ac0f12
2b2t_names <minecraftdrpurple1@outlook.com>
2025-12-30 15:54:21 +0300

    Initial Commit

---
+++ src/main/java/me/tiger/spamShield/SpamShield.java
@@ -0,0 +1,17 @@
+package me.tiger.spamShield;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpamShield extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
